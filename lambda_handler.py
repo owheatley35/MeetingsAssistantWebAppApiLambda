a7beb4457ecd1cd11ea4cf1f2bd97820d7871286
Oli Wheatley <owheatley35@gmail.com>
2022-09-14 02:32:49 +0100

    Adding query to add new roles to db

--- lambda_handler.py
+++ lambda_handler.py
@@ -2,10 +2,20 @@ from data.provider.DBConfigurationProvider import DBConfigurationProvider
 from data.provider.UserRoleProvider import UserRoleProvider
 from database.DatabaseConnectionHelper import DatabaseConnectionHelper
 from database.MySQLQueryExecutor import MySQLQueryExecutor
+# from lambda_event import LambdaEvent
+from helper.LoggingHelper import LoggingHelper
+from response.ResponseCreator import ResponseCreator
+# from response.SetResponses import SetResponses
+# from routing.endpoint_router import EndpointRouter
+# from security.UserAuthoriser import UserAuthorizer
+# from security.exceptions.AuthError import AuthError
+
+logger = LoggingHelper("Handler").retrieve_logger()
 
 
 def handle(context, event):
-    print("Handling From")
+    global current_user_id
+    # logger.info("Handling")
 
     # create_db_beta = """CREATE DATABASE meetingsassistant"""
     #
@@ -14,35 +24,50 @@ def handle(context, event):
     # RoleName varchar(255) NOT NULL);"""
     #
     update_with_inital_user = """INSERT into meetingsassistant.users (UserId, RoleName)
-    VALUES ('621694e858c5f70069b7cb06', 'role:admin');"""
-    #
+    VALUES ('624c0fe938bf3900699ac5cc', 'role:admin');"""
+
+    update_with_user = """INSERT into meetingsassistant.users (UserId, RoleName)
+        VALUES ('624c117bb407b20069e31c01', 'role:standard');"""
+
     # select_tables = """SELECT * FROM meetingsassistant.users"""
-    #
-    # # db_connection = DatabaseConnectionHelper()
-    #
+
+    # db_connection = DatabaseConnectionHelper()
+
     db_config = DBConfigurationProvider().get_configuration()
     connection_helper = DatabaseConnectionHelper(db_config)
-    #
+
     if connection_helper.is_connection_open():
         query_helper = MySQLQueryExecutor(connection_helper.get_connection_cursor())
         # query_helper.execute_query(create_db_beta)
         # query_helper.execute_query(initial_sql_setup)
         query_helper.execute_query(update_with_inital_user)
+        query_helper.execute_query(update_with_user)
 
     connection_helper.commit_connection()
     connection_helper.close_connection()
 
-    retrieve_rows = UserRoleProvider('621694e858c5f70069b7cb06')
-    print(retrieve_rows.get_user_role())
+    # Authenticate User
+
+    # lambda_event = LambdaEvent(event)
+    #
+    # try:
+    #     user = UserAuthorizer(lambda_event.get_header()).authorise_user()
+    # except AuthError as e:
+    #     logger.error(e)
+    #     return SetResponses.UNAUTHENTICATED
+    #
+    # # Route request to desired endpoint
+    # endpoint_router = EndpointRouter(user, lambda_event.get_request_path(), lambda_event.get_query_parameters())
+    # endpoint_response = endpoint_router.route_endpoint()
+
+    retrieve_rows = UserRoleProvider('624c0fe938bf3900699ac5cc')
+    logger.debug(retrieve_rows.get_user_role())
+    retrieve_rows.finish()
+
+    retrieve_rows_two = UserRoleProvider('624c117bb407b20069e31c01')
+    logger.debug(retrieve_rows.get_user_role())
+    retrieve_rows_two.finish()
 
     # Verify user and then call endpoint router. Take result and return it in a formatted model
 
-    return {
-        "statusCode": 200,
-        "headers": {
-            "Content-Type": "application/json"
-        },
-        "body": {
-            "message": "Complete"
-        }
-    }
+    return ResponseCreator({"message": "Complete"}).generate_successful_response()
